85c437c102ea879caef35323f209f7685a77ed87
211125 <84791176+211125@users.noreply.github.com>
2022-06-27 23:49:59 -0500

    Delete ICommentsRepository.java

--- src/main/java/com/escuelita/demo/repositories/ICommentsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.escuelita.demo.repositories;
-
-import com.escuelita.demo.entities.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICommentsRepository extends JpaRepository<Comment,Long> {
-
-}
